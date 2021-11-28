8abb50d6cb70edc00a72a7aa5f7de4b514553031
Helder FIlho <elderaf@hotmail.com>
2021-11-28 18:17:21 -0300

    db

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://koimedia-frontend.herokuapp.com')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next()
