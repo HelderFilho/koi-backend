c12458ccf286c454cca8f45a1aa299d2daa1e4b6
Helder FIlho <elderaf@hotmail.com>
2021-11-28 23:06:09 -0300

    updated

--- src/api/user/getAllUsers.js
+++ src/api/user/getAllUsers.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 exports.get = async (req, res, next) => {
   
-    let db = await database.conn();
+    let db = await database.
     let users = await db.query(`select 
     tu.id_user,
     tu.name, 
@@ -13,9 +13,7 @@ exports.get = async (req, res, next) => {
     tu.place, 
     tu.dt_birthday, 
     tu.fk_id_role, 
-    tu.profile_pic, 
-    (select JSON_ARRAYAGG(truv.fk_id_vehicle) from tb_rel_user_vehicle truv where truv.fk_id_user = tu.id_user) as fk_id_vehicle,
-    (select JSON_ARRAYAGG(tv.fancy_name) from tb_vehicle tv, tb_rel_user_vehicle truv where truv.fk_id_user = tu.id_user and truv.fk_id_vehicle = tv.id_vehicle) as veiculos
+    tu.profile_pic 
     from tb_user tu where tu.deleted = false
     `)
     res.json(users)
