d1b3d1b11716b6b4fa7f9f65970233561119b22e
HelderFilho <elderaf@hotmail.com>
2022-01-16 16:29:44 -0300

    Update deleteProposal.js

--- src/api/proposal/deleteProposal.js
+++ src/api/proposal/deleteProposal.js
@@ -3,9 +3,10 @@ const bcrypt = require('bcrypt');
 
 
 exports.post = async (req, res, next) => {
-    let {id_square} = req.body
+    console.log('qu', req.body)
+    let {id_proposals} = req.body
     let db = await database.conn();
-    let squares = await db.query(`update tb_square set deleted = true where id_square = ${id_square}`)
+    let squares = await db.query(`update tb_proposals set deleted = true where id_proposals = ${id_proposals}`)
     res.json(squares)
 
     };
