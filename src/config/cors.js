b9b3ad7fe2bd20897a86f66b5bc1482cb8ee48cd
HelderFilho <helder.filho@sensrit.com.br>
2022-04-16 11:07:39 -0300

    Update cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,6 +3,7 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     res.header("Access-Control-Expose-Headers", "Content-Security-Policy, Location");
+    res.header("Access-Control-Allow-Credentials", "true")
     next()
   }
   
