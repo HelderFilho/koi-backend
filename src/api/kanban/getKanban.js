b2a67d4f77f620c0479c26eb4cfecb3f44fb400f
HelderFilho <elderaf@hotmail.com>
2022-07-24 18:33:16 -0300

    update

--- src/api/kanban/getKanban.js
+++ src/api/kanban/getKanban.js
@@ -8,44 +8,12 @@ const files = require('../../utils/filesUtils')
 exports.post = async (req, res, next) => {
     let { user } = req.body
     let db = await database.conn();
-    let kanban = []
-    let otherCards = await db.query(`
-    select tkc.*
-    from tb_kanban_card tkc, tb_rel_user_kanban_card trukc where
-tkc.id_kanban_card = trukc.fk_id_kanban_card and trukc.fk_id_user = ${user} and tkc.deleted = false
-    `)
-    if (otherCards[0].length > 0) {
-        let anotherKanban = {
-            id_kanban_column : 0,
-            user_cad : 0,
-            name : 'Cards enviados a você',
-            cards : otherCards[0]
-        }
-        kanban.push(anotherKanban)
-    }
+    let cards = await db.query(`select tc.*,
+    (select fancy_name from tb_client where tb_client.id_client = tc.fk_id_client limit 1) as name_client,
+    (select name from tb_user where tb_user.id_user = tc.user_cad limit 1) as name_user,
 
-    let yourCards = await db.query(`
-    select *,
-    (select JSON_ARRAYAGG(JSON_OBJECT(
-        'subject', tkc.subject, 
-        'description', tkc.description, 
-        'id_kanban_card', tkc.id_kanban_card,
-        'user_cad', tkc.user_cad
-        
-        ))
-    from tb_kanban_card tkc where tkc.user_cad = ${user} and tkc.fk_id_kanban_column = tkc2.id_kanban_column and tkc.deleted = false) as cards
-  
-    from tb_kanban_column tkc2 where user_cad = ${user}  and tkc2.deleted = false`)
-
-    kanban.push(...yourCards[0])
-        await Promise.all(kanban.map( async k => {
-            let cards = k.cards ? k.cards : []
-            await Promise.all(  cards.map(async c => {
-                let users = await db.query(`
-                select fk_id_user from tb_rel_user_kanban_card trukc where trukc.fk_id_kanban_card = ${c.id_kanban_card}`)
-                c.users = users[0].map(u => u.fk_id_user)
-            }))
-        }))
-    res.json([kanban])
+    (select fancy_name from tb_agency where tb_agency.id_agency = tc.fk_id_agency limit 1) as name_agency
+    from tb_cards tc where tc.deleted = false`)
+    res.json(cards[0])
 
 };
